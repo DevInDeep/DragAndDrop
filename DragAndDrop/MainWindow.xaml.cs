65de78a36dc8c59e21ba08f06a5c984eee3a9dcd
vpavlevski <vpavlevski@yahoo.com>
2024-09-29 08:51:47 +0200

    Add project files.

---
+++ DragAndDrop/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace DragAndDrop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void File_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = e.Data.GetData(DataFormats.FileDrop) as string[];
+                if (filePaths != null && filePaths.Length > 0)
+                {
+                    foreach(string filePath in filePaths)
+                        filesList.Items.Add(filePath);
+                }
+            }
+        }
+    }
+}
